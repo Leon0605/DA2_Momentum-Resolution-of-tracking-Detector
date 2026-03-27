c961427c60a11fc2c25db4a87b4991d6307586c3
Mike Buder <buder.mike@gmail.com.com>
2026-03-27 09:49:18 +0100

    added part 4 with experiment setup

--- Project.py
+++ Project.py
@@ -1,5 +1,5 @@
 import numpy as np
-import math
+import matplotlib as plt
 
 class particle:
 
@@ -10,6 +10,20 @@ class particle:
         self.layers = None
 
 def cellsHit(dZ,n,z):
+    pass
 
 
+
+### 4 Momentum Resolution
+
+# Experiment Setup
+n_before = 5    # number of detection planes before magnet
+n_after = 3     # number of detection planes after magnet
+delta_z = 2     # distance detector planes
+width_cell = 5  # width of detection plane cells in 100 mikrometers (1mm is 10 then, 1cm is 100)
+L = 1000        # Magnet Lenght
+B = 0.5         # strength of magnetic field in T
+
+
+# Warm-up Exercise
 
